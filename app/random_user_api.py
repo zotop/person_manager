06b3f9651c2b92c1d67dff5302f866297742def2
Joao Guerreiro <jmdguerreiro@gmail.com>
2018-02-25 22:04:16 +0100

    wip: randomuser api integration

---
+++ app/random_user_api.py
@@ -0,0 +1,7 @@
+API_ENDPOINT = "https://randomuser.me/api/"
+
+class RandomUserApi:
+
+    #TODO
+    def get_random_user_as_person():
+        return None
---
+++ tests/test_random_user_api.py
@@ -0,0 +1,4 @@
+import pytest
+
+#TODO
+def test_get_random_user_as_person():
