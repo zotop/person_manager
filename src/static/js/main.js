b5b57353d970b03ce3182fcee1a4939eeb22ac73
Joao Guerreiro <jmdguerreiro@gmail.com>
2018-02-25 18:52:38 +0100

    removing a person

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,10 +1,36 @@
+function remove_person(personRow, personId) {
+  $.ajax({
+    url: '/api/persons/' + personId,
+    type: 'DELETE',
+    success: function(response){
+      console.log(response);
+      $(personRow).remove();
+    },
+    error: function(error){
+      console.log(error);
+      alert("Error: Could not remove person.");
+    }
+  });
+}
+
+function create_person_row(person) {
+  var personRow = $("<tr person_id='" + person['id']+ "'><td>" +
+  person['first_name']+ "</td><td>" + person['last_name'] + "</td>" +
+  "<td><button>Delete</button></td>" + "</tr>");
+  $(personRow).find('button').click(function(){
+    remove_person(personRow, person['id']);
+  });
+  return personRow;
+}
+
+
 $(window).on( "load", function() {
    $.ajax({
 			url: '/api/persons/list',
 			type: 'GET',
 			success: function(persons){
         $.each( persons, function( index, person ) {
-          personRow = "<tr><td>" + person['first_name']+ "</td><td>" + person['last_name'] + "</td></tr>";
+          var personRow = create_person_row(person);
           $('#personsTable tbody').append(personRow);
         });
 			},
@@ -25,7 +51,8 @@ $(function(){
       contentType: "application/json",
 			type: 'POST',
 			success: function(person){
-        newPersonRow = "<tr><td>" + person['first_name']+ "</td><td>" + person['last_name'] + "</td></tr>";
+        console.log(person);
+        var newPersonRow = create_person_row(person);
         $('#personsTable tbody').prepend(newPersonRow);
 			},
 			error: function(error){
