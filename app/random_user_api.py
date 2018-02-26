8d2af6ab99cc11286b0f75a95ed3fe9e6c9b1be0
Joao Guerreiro <jmdguerreiro@gmail.com>
2018-02-26 22:03:19 +0100

    calling and extracting a person from randomuser api

--- app/random_user_api.py
+++ app/random_user_api.py
@@ -1,7 +1,14 @@
-API_ENDPOINT = "https://randomuser.me/api/"
+import requests
+
+API_ENDPOINT = "https://randomuser.me/api"
 
 class RandomUserApi:
 
-    #TODO
-    def get_random_user_as_person():
-        return None
+    def get_random_person(self):
+        response = requests.get(API_ENDPOINT)
+        user_json = response.json()
+        name = user_json['results'][0]['name']
+        first_name = name['first'].title()
+        last_name = name['last'].title()
+        person_dict = dict(first_name=first_name,last_name=last_name)
+        return person_dict
--- tests/test_random_user_api.py
+++ tests/test_random_user_api.py
@@ -1,5 +1,9 @@
+from app.random_user_api import RandomUserApi
 import pytest
 
+#TODO
+def test_get_random_person():
+    person = RandomUserApi().get_random_person()
 
-def test_get_random_user_as_person():
-    assert False
+    assert len(person['first_name']) > 0
+    assert len(person['last_name']) > 0
